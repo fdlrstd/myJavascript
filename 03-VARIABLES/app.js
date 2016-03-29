d17b6ed5039643f18931f2a8deb6ef260471f472
fdlrstd <jongofidel@yahoo.fr>
2016-03-29 15:26:41 +0200

    do1

---
+++ 03-VARIABLES/app.js
@@ -0,0 +1,87 @@
+/*
+	# Exercise : Diseur de bonne aventure
+
+	Déclarez les variables suivantes: nombre d'enfants, nom d'une femme, un pays, métier.
+	afficher dans la console : "Vous serez X et habiterez à Y, et marié a Z avec N enfants."
+*/
+
+var nbChild = 3, wifeNam = "Laura", countryNam = "Cuba", workNam = "Dev"; 
+console.log("vous serez "+ workNam +" et habiterz à " +countryNam +", et marié a "+ wifeNam +" avec "+ nbChild +" enfants");
+
+
+/*
+	# Exercise : Calculer l'age
+
+	Déclarez les variables suivantes : 
+		- l'année courante
+		- l'année de naissance d'une personne
+
+	Calculer l'age de cette personne
+*/
+
+
+var currentDat = 2016; bDat = 1991;
+var result;
+result = currentDat  - bDat;
+
+console.log("il a: " + result)
+
+/*
+
+	# Exercise : bientôt vieux
+
+	- Déclarez une variable avec votre age
+	- Déclarez une variable avec un a age maximum ( esperons tous vivre 100ans )
+	- Déclarez une variable avec une denrée alimentaire
+	- Déclarez une variable avec votre consomation de cette denrée par jour ( int )
+
+	Calculer le nombre de fois que vous en mangerez pour le restant de votre vie
+	Mettez le resultat dans la console comme ceci : Il vous reste N de X avant d'ateindre l'age de Y ans";
+*/
+
+
+var age = 32, bdateMax = 100, dAlim = "burger", qteConsom = 2;
+
+console.log("Il vous reste N de X avant d'ateindre l'age de Y ans");
+
+
+/*
+	# Exercise : 
+
+	- Décomposer la résolution du calcul suivant : 
+
+	var result = (1 + 2) * 3 + 4 / 2;
+*/
+var result = (1 + 2) * 3 + 4 / 2;
+var result = (3) * 3 + 4 / 2;
+var result = 9+ 4 / 2;
+var result = 9+ 2;
+var result = 11;
+console.log(result);
+/*
+var a, b , c, d;
+//-> etape 1 :
+a = 1+2;
+console.log(1+2);
+console.log("---> " + a);
+//-> etape 2 : 
+b = a * 3;
+console.log((1+2)*3);
+console.log("---> " + b);
+//-> etape 3 :
+c = 4/2;
+console.log(4/2);
+console.log("---> " + c);
+//-> etape 4 :
+d = b + c;
+console.log((1+2)*3 + 4/2);
+console.log("---> " + d);
+
+*/
+
+
+
+
+
+
+
